382c8b21e76aa9ddf885a73fe322e5804fd7bd49
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-08-08 22:48:06 +0300

    ADD: auth store

---
+++ src/store/auth/operations.ts
@@ -0,0 +1,97 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+axios.defaults.baseURL = "https://plan-back.onrender.com";
+
+const setAuthHeader = token => {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+const clearAuthHeader = token => {
+    axios.defaults.headers.common.Authorization = '';
+};
+
+/*
+ * POST @ /users/signup
+ * body: { name, email, password }
+ */
+export const register = createAsyncThunk(
+    '/users/register',
+    async (credentials, thunkAPI) => {
+        try {
+            const response = await axios.post(
+                '/users/register',
+                credentials
+            );
+            setAuthHeader(response.data.token);
+            return response.data;
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.message);
+        }
+    }
+);
+
+/*
+ * POST @ /users/login
+ * body: { email, password }
+ */
+export const login = createAsyncThunk(
+    '/users/login',
+    async (credentials, thunkAPI) => {
+        try {
+            const response = await axios.post(
+                '/users/login',
+                credentials
+            );
+            setAuthHeader(response.data.token);
+            return response.data;
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.message);
+        }
+    }
+);
+
+/*
+ * POST @ /users/logout
+ * headers: Authorization: Bearer token
+ */
+export const logout = createAsyncThunk(
+    '/users/logout',
+    async (_, thunkAPI) => {
+        try {
+            const response = await axios.post(
+                '/users/logout'
+            );
+            // clearAuthHeader();
+            return response.data;
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.message);
+        }
+    }
+);
+
+/*
+ * GET @ /users/current
+ * headers: Authorization: Bearer token
+ */
+export const refreshUser = createAsyncThunk(
+    '/users/current',
+    async (_, thunkAPI) => {
+        const state = thunkAPI.getState();
+        const persistedToken = state.auth.token;
+
+        if (persistedToken === null) {
+            return thunkAPI.rejectWithValue('Unable to fetch user');
+        }
+
+        try {
+            setAuthHeader(persistedToken);
+            const response = await axios.get(
+                '/users/current'
+            );
+            return response.data;
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.message);
+        }
+    }
+);
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,44 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit"
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { authReduser } from "./auth/authSlice";
 
+
+
+const middleware = [
+    ...getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        }
+    })
+];
+
+const authPersistConfig = {
+    key: 'auth',
+    storage,
+    whitelist: ['token'],
+};
 
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        auth: persistReducer(authPersistConfig, authReduser),
+    },
+    middleware,
+    devTools: process.env.NODE_ENV === 'development',
 });
 
+
+export const persistor = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
